c21c2e9867e46e79cc76650925dc2d754a1a4660
Fernando Ruiz <fruiz@wellcom.com.mx>
2022-02-01 23:59:25 -0600

    se crea controlador de mapbox

--- app/Http/Controllers/MapboxController.php
+++ app/Http/Controllers/MapboxController.php
@@ -7,6 +7,20 @@ use Illuminate\Http\Request;
 class MapboxController extends Controller
 {
     //
+    public function Geocoding(Request $request){
 
+        $request->validate([
+            'codigo_postal' => 'required|numeric|digits:5'
+        ],[
+            'codigo_postal.required' => 'El campo código postal es obligatorio',
+            'codigo_postal.numeric' => 'El campo código postal debe ser numérico',
+            'codigo_postal.digits' => 'El campo código postal debe tener 5 dígitos'
+        ]);
+
+        $codigo_postal = $request->codigo_postal;
+
+        
+
+    }
     
 }
