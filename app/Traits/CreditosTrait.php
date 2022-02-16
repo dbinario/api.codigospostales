ba6b5b8d52907daec07a9113927945bc12afc836
Fernando Ruiz <fruiz@wellcom.com.mx>
2022-02-16 00:14:38 -0600

    se actualiza a laravel 9

--- app/Traits/CreditosTrait.php
+++ app/Traits/CreditosTrait.php
@@ -5,7 +5,7 @@ use App\Models\User;
 
 trait CreditosTrait
 {
-    public function DescontarCreditos($user, $cantidad)
+    public static function DescontarCreditos($user, $cantidad)
     {
         $user = User::find($user);
         $user->creditos = $user->creditos - $cantidad;
