0b796cae8f55fd607fc6e38a38f67212740a0510
Fernando Ruiz <fruiz@wellcom.com.mx>
2022-02-27 13:45:16 -0600

    se ajusta el guardado de las peticiones

--- app/Traits/PeticionesTrait.php
+++ app/Traits/PeticionesTrait.php
@@ -4,11 +4,14 @@ namespace App\Traits;
 
 use Illuminate\Http\Request;
 use App\Models\Peticiones;
+use Illuminate\Support\Arr;
 
 trait PeticionesTrait
 {
     public function GuardarPeticiones(Request $request)
     {
+
+        //convertimos la peticion a json
 
         if ($request->all()==null) {
             $datos_enviados=json_encode('');
@@ -16,7 +19,11 @@ trait PeticionesTrait
             $datos_enviados=json_encode($request->all());
         }
 
+        //quitamos los campos que no queremos guardar
+        $datos_enviados=json_encode(Arr::except($request->all(), ['api_key']));
 
+
+        //guardamos la peticion en la base de datos
         $peticion = new Peticiones;
         $peticion->ip_address = $request->ip();
         $peticion->metodo = $request->method();
