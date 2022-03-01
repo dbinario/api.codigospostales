e9951b01bc30a890bd4bef4043d6070920c389e8
Fernando Ruiz <fruiz@wellcom.com.mx>
2022-03-01 00:02:26 -0600

    se ajustan las rutas

--- routes/api-web.php
+++ routes/api-web.php
@@ -10,12 +10,17 @@ use App\Http\Controllers\ApiKeyController;
 //registrar usuario
 Route::post('registrar_usuario', [UsuariosController::class,'RegistrarUsuario'])->name('Registrar Usuario');
 
-
-//autenticar usuario
+//autenticar usuario para recuperar token
 Route::post('autenticar_usuario', [UsuariosController::class,'AutenticarUsuario'])->name('Autenticar Usuario');
 
+
+//proteger rutas con token de sanctum
+Route::middleware(['auth:sanctum'])->group(function () {
+
 //solicitar api key se requiere token de usuario para consumirlo
-Route::post('crear_apikey', [ApiKeyController::class,'GenerarApiKey'])->name('Crear ApiKey')->middleware('auth:sanctum');
+Route::post('crear_apikey', [ApiKeyController::class,'GenerarApiKey'])->name('Crear ApiKey');
 
 //sumar creditos
-Route::post('sumar_creditos',[UsuariosController::class,'SumarCreditos'])->name('Sumar Creditos')->middleware('auth:sanctum');
+Route::post('sumar_creditos',[UsuariosController::class,'SumarCreditos'])->name('Sumar Creditos');
+
+});
